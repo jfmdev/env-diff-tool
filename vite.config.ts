b04d180d5d6d45c9374f3941d83c1e8de5d695ec
Jose <jfmdev@gmail.com>
2025-07-31 19:25:14 -0300

    Add PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Env Diff Tool',
+        short_name: 'Env Diff',
+        icons: [
+          { src: 'icons/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icons/icon-512.png', sizes: '512x512', type: 'image/png' },
+        ],
+        // TODO: Add other manifest properties like start_url, display, background_color, etc.
+      },
+      // TODO: Configure service worker strategies (e.g., generateSW, injectManifest)
+    }),
+  ],
 })
